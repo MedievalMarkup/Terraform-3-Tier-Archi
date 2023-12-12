42d98c4c3d948b5cc21574b2e275099efd496a1d
Adil Nehal <adil.nehal@ai.local>
2023-12-12 19:39:49 +0500

    adding modules

--- manual/modules/subnets/main.tf
+++ manual/modules/subnets/main.tf
@@ -1,6 +1,7 @@
 resource "aws_subnet" "aws_subnets" {
+  for_each = toset(var.sub_cidr)
   vpc_id = var.aws_vpc_id
-  cidr_block = var.sub_cidr
+  cidr_block = each.key
   availability_zone = var.sub_av_zone
   tags = {
     subnet_type = var.subnet_type
