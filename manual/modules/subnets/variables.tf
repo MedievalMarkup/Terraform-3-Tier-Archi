af641a6f3b5fb8229448d77da993fa6f33b7d2c1
Adil <adilnehal2000@gmail.com>
2023-12-13 13:38:51 +0500

    initial

--- manual/modules/subnets/variables.tf
+++ manual/modules/subnets/variables.tf
@@ -10,14 +10,10 @@ variable "sub_av_zone" {
 
 variable "sub_cidr" {
   description = "vpc id"
-  type = string
+  type = list(string)
 }
 
-<<<<<<< HEAD
 
-
-=======
->>>>>>> f0b6ec4a4a2ab3b4aea515e6a625477564d5eb89
 variable "subnet_type" {
   description = "vpc id"
   type = string
