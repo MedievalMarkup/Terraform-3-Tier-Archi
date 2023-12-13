7b37307196ad432d0bd8b9e8ebaec8fecaa88ea2
Adil Nehal <adil.nehal@ai.local>
2023-12-13 16:39:41 +0500

    adding modules

--- manual/variables.tf
+++ manual/variables.tf
@@ -24,17 +24,17 @@ variable "list_private_subnets" {
     {
       subnet_type = "private"
       cidr_block = ["10.0.1.0/24", "10.0.2.0/24"]
-      av_zone = "us-west-1"
+      av_zone = "us-west-1a"
     },
     {
       subnet_type = "database"
       cidr_block = ["10.0.151.0/24", "10.0.152.0/24"]
-      av_zone = "us-west-1"
+      av_zone = "us-west-1a"
     },
     {
       subnet_type = "public"
       cidr_block = ["10.0.101.0/24", "10.0.102.0/24"]
-      av_zone = "us-west-1"
+      av_zone = "us-west-1a"
     }
   ]
 }
