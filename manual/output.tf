4d9556a8d24bf8aafa70f4ef7e7d62d84f23fd82
Adil Nehal <adil.nehal@ai.local>
2023-12-14 17:28:18 +0500

    subnet_ids

--- manual/output.tf
+++ manual/output.tf
@@ -2,12 +2,8 @@ output "vpc_id" {
   value = module.VPC-Manual.vpc_id
 }
 
-# output "id_private_subnets" {
-#   value = [for id in module.subnets: id.subnet_private_id]
-# }
-
 output "id_private_subnets" {
-  value = module.subnets.subnet_private_id
+  value = [for id in module.subnets: id.subnet_private_id != null ? id.subnet_private_id : null]
 }
 
 output "id_public_subnets" {
