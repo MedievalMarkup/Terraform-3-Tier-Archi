7ef7e3739942b890ed05f1bab4b5f4269cd16ab3
Adil Nehal <adil.nehal@ai.local>
2023-12-17 18:46:17 +0500

    subnet_ids

--- manual/main.tf
+++ manual/main.tf
@@ -4,14 +4,14 @@ module "VPC-Manual" {
   vpc_cidr_block = var.vpc_cidr_block
 }
 
-# module "subnets" {
-#   source = "./modules/subnets"
-#   for_each = { for id, subnet in local.get_product: id => subnet }
-#   aws_vpc_id = module.VPC-Manual.vpc_id
-#   sub_cidr = each.key
-#   sub_av_zone = each.value
-#   subnet_type = local.final_subnet_type
-# }
+module "subnets" {
+  source = "./modules/subnets"
+  for_each = { for id, subnet in local.get_product: id => subnet }
+  aws_vpc_id = module.VPC-Manual.vpc_id
+  sub_cidr = each.key
+  sub_av_zone = each.value
+  subnet_type = local.final_subnet_type
+}
 
 # module "NAT-Gateway" {
 #   source = "./modules/NAT-Gateway"
