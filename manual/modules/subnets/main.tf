201422948b97342fd5a9c1e46d63fe8ec408dc24
Adil Nehal <adil.nehal@ai.local>
2023-12-17 15:40:33 +0500

    subnet_ids

--- manual/modules/subnets/main.tf
+++ manual/modules/subnets/main.tf
@@ -1,8 +1,8 @@
 resource "aws_subnet" "aws_subnets" {
-  for_each = toset(var.sub_cidr)
+  # for_each = toset(var.sub_cidr)
   vpc_id = var.aws_vpc_id
-  cidr_block = each.key
-  availability_zone = each.key
+  cidr_block = var.sub_cidr
+  availability_zone = var.sub_av_zone
   tags = {
     subnet_type = var.subnet_type
   }
