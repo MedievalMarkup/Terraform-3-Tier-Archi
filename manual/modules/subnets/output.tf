4364c4cf3f76496baae8e5e747e2a328816d4a28
Adil Nehal <adil.nehal@ai.local>
2023-12-24 16:57:34 +0500

    initial

--- manual/modules/subnets/output.tf
+++ manual/modules/subnets/output.tf
@@ -1,5 +1,4 @@
 
-output "public_subnet_id" {
-  value = [for subnet in aws_subnet.aws_subnets : 
-              subnet.tags != null && contains(keys(subnet.tags), "subnet_type") && subnet.tags["subnet_type"] == "public" ? subnet.id : null]
+output "public_subnet_ids" {
+  value = values(module.subnets)[*].id 
 }
