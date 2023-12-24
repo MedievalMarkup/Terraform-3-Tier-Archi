a228bed1e35d7a03d626032cd4905b5ca385974d
Adil Nehal <adil.nehal@ai.local>
2023-12-24 15:27:32 +0500

    initial

--- manual/modules/subnets/output.tf
+++ manual/modules/subnets/output.tf
@@ -1,13 +1,8 @@
 output "subnet_type_id" {
-  value = "${[for subnet in aws_subnet.aws_subnets : "${subnet.tags["subnet_type"]} = ${subnet.id}"]}"
+  value = [for subnet in aws_subnet.aws_subnets : subnet.tags["subnet_type"] == "private" ? subnet.id : null]
 }
 
-
-
 # output "subnet_public_id" {
 #   value = "${[for subnet in aws_subnet.aws_subnets : subnet.tags["subnet_type"] == "public" ? subnet.id : null]}"
 # }
 
-# output "subnet_public_id" {
-#   value = "${[for subnet in aws_subnet.aws_subnets : subnet.tags["subnet_type"] == "public" ? subnet.id : null]}"
-# }
