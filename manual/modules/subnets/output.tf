d696f97535e2ebd5ed9e708bc5870410850fe9c3
Adil Nehal <adil.nehal@ai.local>
2023-12-24 15:49:28 +0500

    initial

--- manual/modules/subnets/output.tf
+++ manual/modules/subnets/output.tf
@@ -1,8 +1,8 @@
-output "subnet_type_id" {
-  value = [for subnet in aws_subnet.aws_subnets : subnet]
+output "subnet_obj" {
+  value = [for subnet in aws_subnet.aws_subnets: subnet]
 }
 
-# output "subnet_public_id" {
-#   value = "${[for subnet in aws_subnet.aws_subnets : subnet.tags["subnet_type"] == "public" ? subnet.id : null]}"
-# }
 
+# output "public_subnet_id" {
+#   value = [for subnet in aws_subnet.aws_subnets: subnet.tags["subnet_type"] == "public" ? subnet.id : null]
+# }
--- manual/output.tf
+++ manual/output.tf
@@ -2,7 +2,7 @@ output "vpc_id" {
   value = module.VPC-Manual.vpc_id
 }
 
-output "private_id" {
+output "subnet_obj" {
   value = [for id in module.subnets.subnet_type_id : id]
 }
 
