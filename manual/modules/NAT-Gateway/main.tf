80886114c395390fb94b8647b1e52e883ca25f50
Adil Nehal <adil.nehal@ai.local>
2023-12-25 20:03:13 +0500

    adding NAT & INTERNET

--- manual/modules/NAT-Gateway/main.tf
+++ manual/modules/NAT-Gateway/main.tf
@@ -1,5 +1,5 @@
 resource "aws_nat_gateway" "test-nat" {
-  for_each = tomap(var.public_ids)
+  for_each = {for id, subnet in var.public_ids: id => subnet}
   allocation_id = var.eip_alloc_id
   subnet_id = each.key
 }
