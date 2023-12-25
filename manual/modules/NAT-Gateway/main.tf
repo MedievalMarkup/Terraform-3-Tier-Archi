854f170054162095c48e3bbe98d7eccb2641314d
Adil Nehal <adil.nehal@ai.local>
2023-12-25 20:18:09 +0500

    adding NAT & INTERNET

--- manual/modules/NAT-Gateway/main.tf
+++ manual/modules/NAT-Gateway/main.tf
@@ -1,5 +1,15 @@
+# Define a static map for for_each with known keys
+locals {
+  public_ids_map = {
+    subnet1 = var.public_ids[0]
+    subnet2 = var.public_ids[1]
+    # Add statically defined keys for each public ID in var.public_ids
+    # Adjust the keys according to your actual requirements
+  }
+}
+
 resource "aws_nat_gateway" "test-nat" {
-  for_each = {for id, subnet in var.public_ids: id => subnet}
-  allocation_id = var.eip_alloc_id
-  subnet_id = each.key
+  for_each = local.public_ids_map
+  allocation_id = var.eip_alloc_id[each.key]
+  subnet_id     = each.value
 }
